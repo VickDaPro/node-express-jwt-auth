af71cbfb0d2674304c6e27077318512285c4252d
VickDaPro <vickbishnoi@gmail.com>
2024-04-20 17:07:24 +0530

    Created a fully functional jwt authentication using node.js, express and mongodb with ejs as views

---
+++ README.md
@@ -0,0 +1,11 @@
+Features:
+
+1. Followed industry standard MVC(Model view controller) architecture.
+2. Used EJS for frontend inside nodejs only.
+3. Set up middlewares for converting data to json and cookie parsing.
+4. Using .env to protect sensitive information from reaching to github.
+5. Server side validation.
+6. Password encryption using bcrypt before saving them to the database.
+7. Set up JWT for authentication.
+8. Set up middleware for protected routes.
+9. Sign up, Login and Logout functionality with proper token verification.
---
+++ models/User.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Please enter an email"],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter a password"],
+    minlength: [6, "Minimum password length is 6 characters."],
+  },
+});
+
+// Mongoose middlewares
+
+// Fire a function after a doc saved to db
+// userSchema.post("save", function (doc, next) {
+//   console.log("A new user is created & saved", doc);
+//   next();
+// });
+
+// Fire a function before a doc saved to db
+// userSchema.pre("save", function (next) {
+//   console.log("A new user is about to be created & saved", this);
+//   next();
+// });
+
+// Fire a function before a doc saved to db
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Static method to login user
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error("Incorrect password");
+  }
+  throw Error("Incorrect email");
+};
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
